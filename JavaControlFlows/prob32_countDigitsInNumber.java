c873dcf374ba271c84edca3a8bb3035ec799e3bc
Dipanshuk1 <dipanshukumar1936@gmail.com>
2025-06-17 12:26:12 +0530

    Added program to count number of digits in an integer

---
+++ JavaControlFlows/prob32_countDigitsInNumber.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class prob32_countDigitsInNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter an integer: ");
+        int number = sc.nextInt();
+
+        int count = 0;
+        int temp = Math.abs(number);  // Handle negative numbers
+
+        if (temp == 0) {
+            count = 1;  // Zero has one digit
+        } else {
+            while (temp != 0) {
+                temp /= 10;  // Remove last digit
+                count++;
+            }
+        }
+
+        System.out.println("Number of digits: " + count);
+    }
+}
