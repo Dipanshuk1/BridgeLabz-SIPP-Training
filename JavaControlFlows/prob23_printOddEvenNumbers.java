6fb0334402c2fcd04beb59547e96366dea3fc172
Dipanshuk1 <dipanshukumar1936@gmail.com>
2025-06-17 12:16:28 +0530

    Added program to print odd and even numbers up to a given number

---
+++ JavaControlFlows/prob23_printOddEvenNumbers.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class prob23_printOddEvenNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a natural number: ");
+        int number = sc.nextInt();
+        if (number <= 0) {
+            System.out.println("Invalid input. Enter a natural number.");
+            return;
+        }
+
+        for (int i = 1; i <= number; i++) {
+            if (i % 2 == 0)
+                System.out.println(i + " is even");
+            else
+                System.out.println(i + " is odd");
+        }
+    }
+}
