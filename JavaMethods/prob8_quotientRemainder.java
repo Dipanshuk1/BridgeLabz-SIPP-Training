e44bbe5f18c302984d12312a8a13efcd190ee4c5
Dipanshuk1 <dipanshukumar1936@gmail.com>
2025-06-20 10:08:17 +0530

    Calculate quotient and remainder of two numbers

---
+++ JavaMethods/prob8_quotientRemainder.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class prob8_quotientRemainder {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter dividend: ");
+        int number = sc.nextInt();
+        System.out.print("Enter divisor: ");
+        int divisor = sc.nextInt();
+
+        int[] result = findRemainderAndQuotient(number, divisor);
+        System.out.println("Quotient: " + result[0] + ", Remainder: " + result[1]);
+    }
+
+    public static int[] findRemainderAndQuotient(int number, int divisor) {
+        return new int[]{number / divisor, number % divisor};
+    }
+}
