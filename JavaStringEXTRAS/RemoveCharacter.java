b1dd4158fda9883d664317e02504eec8f33e4d3c
Dipanshuk1 <dipanshukumar1936@gmail.com>
2025-07-07 12:22:19 +0530

    Delete JavaStringEXTRAS directory

--- JavaStringEXTRAS/RemoveCharacter.java
+++
@@ -1,8 +0,0 @@
-public class RemoveCharacter {
-    public static void main(String[] args) {
-        String input = "Hello World";
-        char toRemove = 'l';
-        String result = input.replaceAll(String.valueOf(toRemove), "");
-        System.out.println("Modified String: " + result);
-    }
-}
