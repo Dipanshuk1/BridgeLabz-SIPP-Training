2f43cf6856846dd36fbc3afcbd40ea60329c68be
Dipanshuk1 <dipanshukumar1936@gmail.com>
2025-07-12 22:11:09 +0530

    Delete JavaStringEXTRAS directory

--- JavaStringEXTRAS/PalindromeString.java
+++
@@ -1,7 +0,0 @@
-public class PalindromeString {
-    public static void main(String[] args) {
-        String input = "Madam";
-        boolean isPalindrome = input.equalsIgnoreCase(new StringBuilder(input).reverse().toString());
-        System.out.println(isPalindrome ? "Palindrome" : "Not a Palindrome");
-    }
-}
