134f689145c27b51fac8731cd8d344f2110befdc
Dipanshuk1 <dipanshukumar1936@gmail.com>
2025-07-21 08:51:26 +0530

    solve two sum problem using hashmap to store indices

---
+++ Stack,Queue,Hashmap_and_HashingFunction/TwoSumHash.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+
+public class TwoSumHash {
+
+    public static int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int comp = target - nums[i];
+            if (map.containsKey(comp)) {
+                return new int[]{map.get(comp), i};
+            }
+            map.put(nums[i], i);
+        }
+
+        return new int[]{-1, -1}; 
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] res = twoSum(nums, target);
+        System.out.println("Indices: " + res[0] + ", " + res[1]); 
+    }
+}
