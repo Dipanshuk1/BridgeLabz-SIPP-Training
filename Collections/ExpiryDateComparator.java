f1291cf830fdbdc216c22fd715d3fe78e7a0ec63
Dipanshuk1 <dipanshukumar1936@gmail.com>
2025-07-29 08:59:05 +0530

    Added InsurancePolicy class with attributes and constructor for insurance management

--- Collections/ExpiryDateComparator.java
+++
@@ -1,8 +0,0 @@
-package Collections;
-import java.util.*;
-
-public class ExpiryDateComparator implements Comparator<InsurancePolicy> {
-    public int compare(InsurancePolicy p1, InsurancePolicy p2) {
-        return p1.expiryDate.compareTo(p2.expiryDate);
-    }
-}
