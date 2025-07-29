2e5319e792467c57d38ea0fc26a9d406d2811387
Dipanshuk1 <dipanshukumar1936@gmail.com>
2025-07-29 09:02:27 +0530

    Resolved merge conflicts

--- Collections/InsurancePolicy.java
+++ Collections/InsurancePolicy.java
@@ -8,9 +8,9 @@ public class InsurancePolicy {
     String coverageType;
     double premiumAmount;
     public InsurancePolicy(String policyNumber, String policyholderName, Date expiryDate, String coverageType, double premiumAmount) {
-        this.policyNumber = policyNumber;
+        this.policyNumber =policyNumber;
         this.policyholderName = policyholderName;
-        this.expiryDate = expiryDate;
+        this.expiryDate = expiryDate;       
         this.coverageType = coverageType;
         this.premiumAmount = premiumAmount;
     }
