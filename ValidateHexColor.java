6096052eb824d8aa9c66a8bba8bdefcbb0aac3e9
Dipanshuk1 <dipanshukumar1936@gmail.com>
2025-08-27 10:20:33 +0530

    Updated

--- ValidateHexColor.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-public class ValidateHexColor {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String hex = sc.nextLine();
-        System.out.println(hex.matches("^#[0-9A-Fa-f]{6}$") ? "Valid" : "Invalid");
-    }
-}
